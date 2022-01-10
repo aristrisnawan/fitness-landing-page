5a0147ab9c683ce5970dbf96a96d8d1f9defe7bc
aristrisnawan <aristrisnawan35@gmail.com>
2022-01-10 21:42:27 +0700

    Add Card Popular exercise

--- src/components/ExcerciseComponent.js
+++ src/components/ExcerciseComponent.js
@@ -4,8 +4,8 @@ import Excercise from "../image/exercise.png";
 export default function ExcerciseComponent() {
   return (
     <div>
-      <div className="  text-white px-20 h-screen">
-        <div className="flex flex-row bg-gray-500 p-2 w-128 opacity-50 rounded-full mt-28 justify-between">
+      <div className="  text-white px-20">
+        <div className="flex flex-row bg-gray-500 p-2 w-128 opacity-50 rounded-full mt-20 justify-between">
           <button className="bg-green-600 rounded-full text-white py-1 px-4 cursor-pointer">
             NEW
           </button>
@@ -48,6 +48,103 @@ export default function ExcerciseComponent() {
                 Get Started
               </button>
             </div>
+          </div>
+        </div>
+
+        <div className=" mt-40">
+          <PopularExcercise />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function PopularExcercise() {
+  return (
+    <div>
+      <div className="flex flex-row justify-between text-white">
+        <p className=" text-2xl font-bold">Popular Exercises</p>
+        <p className=" text-slate-200 opacity-50 text-xs">SEE MORE EXERCISES</p>
+      </div>
+      <div className=" mt-10 pb-6 grid grid-cols-4 justify-items-center">
+        <div className="mb-5">
+          <div className=" w-60 h-40 rounded relative mb-10">
+            <div className="">
+              <img
+                className="rounded"
+                src="https://media.istockphoto.com/photos/young-female-athlete-running-up-stairs-picture-id1301680726?b=1&k=20&m=1301680726&s=170667a&w=0&h=0yEq5GEY-IKKV7N4WDFpN19G--HqGolwKC9hHawCLrY="
+                alt=""
+              />
+            </div>
+            <div className="absolute -bottom-9 left-0 flex flex-col pl-3">
+              <span className=" text-2xl font-bold">Running</span>
+              <span className=" mt-2 text-xs text-slate-200 opacity-60">
+                250 est calories
+              </span>
+            </div>
+            <span className=" bg-black rounded-tl-md py1 px-2 absolute bottom-0 right-0">
+              58:24
+            </span>
+          </div>
+        </div>
+        <div className="mb-5">
+          <div className=" w-60 h-40 rounded relative">
+            <div className="">
+              <img
+                className="rounded"
+                src="https://media.istockphoto.com/photos/young-female-athlete-running-up-stairs-picture-id1301680726?b=1&k=20&m=1301680726&s=170667a&w=0&h=0yEq5GEY-IKKV7N4WDFpN19G--HqGolwKC9hHawCLrY="
+                alt=""
+              />
+            </div>
+            <div className="absolute -bottom-9 left-0 flex flex-col pl-3">
+              <span className=" text-2xl font-bold">Running</span>
+              <span className=" mt-2 text-xs text-slate-200 opacity-60">
+                250 est calories
+              </span>
+            </div>
+            <span className=" bg-black rounded-tl-md py1 px-2 absolute bottom-0 right-0">
+              58:24
+            </span>
+          </div>
+        </div>
+        <div className="mb-5">
+          <div className=" w-60 h-40 rounded relative">
+            <div className="">
+              <img
+                className="rounded"
+                src="https://media.istockphoto.com/photos/young-female-athlete-running-up-stairs-picture-id1301680726?b=1&k=20&m=1301680726&s=170667a&w=0&h=0yEq5GEY-IKKV7N4WDFpN19G--HqGolwKC9hHawCLrY="
+                alt=""
+              />
+            </div>
+            <div className="absolute -bottom-9 left-0 flex flex-col pl-3">
+              <span className=" text-2xl font-bold">Running</span>
+              <span className=" mt-2 text-xs text-slate-200 opacity-60">
+                250 est calories
+              </span>
+            </div>
+            <span className=" bg-black rounded-tl-md py1 px-2 absolute bottom-0 right-0">
+              58:24
+            </span>
+          </div>
+        </div>
+        <div className="mb-5">
+          <div className=" w-60 h-40 rounded relative">
+            <div className="">
+              <img
+                className="rounded"
+                src="https://media.istockphoto.com/photos/young-female-athlete-running-up-stairs-picture-id1301680726?b=1&k=20&m=1301680726&s=170667a&w=0&h=0yEq5GEY-IKKV7N4WDFpN19G--HqGolwKC9hHawCLrY="
+                alt=""
+              />
+            </div>
+            <div className="absolute -bottom-9 left-0 flex flex-col pl-3">
+              <span className=" text-2xl font-bold">Running</span>
+              <span className=" mt-2 text-xs text-slate-200 opacity-60">
+                250 est calories
+              </span>
+            </div>
+            <span className=" bg-black rounded-tl-md py1 px-2 absolute bottom-0 right-0">
+              58:24
+            </span>
           </div>
         </div>
       </div>
